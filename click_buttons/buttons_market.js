5d501a6956c5ac264f8ea0127298e7c0e5d7b258
Bhagya-vishwakarma <157792295+Bhagya-vishwakarma@users.noreply.github.com>
2024-03-24 03:06:28 +0530

    changed link

--- click_buttons/buttons_market.js
+++ click_buttons/buttons_market.js
@@ -62,10 +62,10 @@ function changebutbg(){
     }}
 
     function creator(){
-        window.location.href = "https://aditya-0510.github.io/legal-vizier/creator/creator.html";
+        window.location.href = "https://legalvizier.github.io/Legal-Vizier/creator/creator.html";
     
     }
     function about(){
-        window.location.href = "https://aditya-0510.github.io/legal-vizier/About/index.html";
+        window.location.href = "https://legalvizier.github.io/Legal-Vizier/About/index.html";
     
     }
